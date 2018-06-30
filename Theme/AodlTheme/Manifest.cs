dc0b7ab5b7228101d5616d94f8b2c2655daba6c4
Alex <alexanderlorimer@hotmail.com>
2018-06-30 21:32:45 +0100

    added an AodlTheme that extends TheBlogTheme

---
+++ Theme/AodlTheme/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "AodlTheme",
+    Author = "The Orchard Team",
+    Website = "https://orchardproject.net",
+    Version = "0.0.1",
+    BaseTheme = "TheBlogTheme",
+    Description = "AodlTheme"
+)]
