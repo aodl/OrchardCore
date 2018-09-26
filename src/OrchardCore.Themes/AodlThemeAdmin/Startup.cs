a5cf91fa7b789ed30f8758dfa25332dcaed94497
Alex <alexanderlorimer@hotmail.com>
2018-09-26 20:12:48 +0100

    other AodlThemAdmin changes

--- src/OrchardCore.Themes/AodlThemeAdmin/Startup.cs
+++ src/OrchardCore.Themes/AodlThemeAdmin/Startup.cs
@@ -1,9 +1,4 @@
-using OrchardCore.DisplayManagement.Implementation;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Builder;
-using System;
 using OrchardCore.Modules;
-using Microsoft.AspNetCore.Routing;
 
 namespace AodlThemeAdmin
 {
